08a4037ba52cd7a46fbb08de8b5994187d23e426
Simon Gibbons <simongibbons@gmail.com>
2014-06-17 13:34:03 +0000

    Done Chapter 4

--- README.rst
+++ README.rst
@@ -6,4 +6,7 @@ tutorial [Tango with Django](http://www.tangowithdjango.com/)
 
 As I go along I'll update this README with my progress.
 
-*  Completed Chapter 3
+*  Completed Chapter 3 + Exercises (no problems here)
+*  Completed Chapter 4 + Exercises
+  - What is the best way of structuring templates? c.f. this
+    tutorial and the official one?
--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,13 @@
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("""Rango says hello world!<br>
-                           <a href="about">About</a>
-                        """)
+    context = RequestContext(request)
+    context_dict = {'boldmessage' : "I am bold font from the context"}
+    return render_to_response('rango/index.html', context_dict, context)
 
 def about(request):
-    return HttpResponse("""Rango Says: Here is the about page.<br>
-                           <a href="/rango">Index</a>
-                        """)
+    context = RequestContext(request)
+    return render_to_response('rango/about.html', {}, context )
